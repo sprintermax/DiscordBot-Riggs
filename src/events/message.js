a652b8e8c6ede9f0710d2cb89946c56d13188613
Sprintermax <ericfnoe7@hotmail.com>
2020-08-20 08:19:35 -0300

    Revert "Revert "Revert "changed things"""
    This reverts commit 59e60c75b47fad02be5794207cc82adc5e207419.

--- src/events/message.js
+++ src/events/message.js
@@ -1,5 +1,4 @@
 const DBTools = require(`../scripts/dbtools.js`);
-/*const Leveling = require(`../modules/leveling.js`);*/
 
 module.exports = (client, message) => {
 	if (message.author.bot || !message.guild) return;
@@ -8,22 +7,10 @@ module.exports = (client, message) => {
 
 	DBTools.checkcfg(client, message).then(function(guilddb) {
 		if (!guilddb.prefix) prefix = defaultcfg.prefix; else prefix = guilddb.prefix;
-
-		/*if (guilddb.config.leveling == "on") {
-			Leveling.run(client, message, guilddb)
-		}*/
-
-		if (message.content == `<@${client.user.id}> help`
-		|| message.content == `<@${client.user.id}> ?`
-		|| message.content == `<@!${client.user.id}> help`
-		|| message.content == `<@!${client.user.id}> ?`) {
-			message.channel.send(`${message.author} meu prefixo está configurado para "\`${prefix}\`" nesse servidor, caso queira ver uma lista com meus comandos digite "\`${prefix}help\`"`);
-		}
-
 		if (message.content.indexOf(prefix) !== 0) return;
 
-		const args = message.content.toLowerCase().slice(prefix.length).trim().split(/ +/g);
-		const command = args.shift();
+		const args = message.content.slice(prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
 		const cmd = client.commands.get(command);
 
 		if (cmd) {
--- src/modules/leveling.js
+++
@@ -1,3 +0,0 @@
-module.exports.run = async (client, message, guilddb) => {
-    
-};
